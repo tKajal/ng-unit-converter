34823bcfc32f22c0b875430abd58861329e06d84
tKajal <thkkjl@gmail.com>
2025-04-12 22:59:44 +0530

    updated converter ui

--- src/app/converter/converter.component.ts
+++ src/app/converter/converter.component.ts
@@ -11,24 +11,51 @@ import { TranslatePipe } from '../pipes/translate.pipe';
   imports:[CommonModule, FormsModule, TranslatePipe]
 })
 export class ConverterComponent {
-  pxValue: number = 0;
-  baseFontSize: number = 16;
+
+  pxValue: string = '';
   remValue: string = '';
-  emValue: string = '';
-  percentageValue: string = '';
-  converted: boolean = false;
+  baseFontSize = 16; // You can make this dynamic if needed
 
-  convert() {
-    const px = this.pxValue || 0;
-    const base = this.baseFontSize || 16;
-
-    const rem = (px / base).toFixed(4);
-    const em = rem;
-    const percent = ((px / base) * 100).toFixed(2);
-
-    this.remValue = rem;
-    this.emValue = em;
-    this.percentageValue = percent;
-    this.converted = true;
+  convertToRem() {
+    const px = parseFloat(this.pxValue);
+    if (!isNaN(px)) {
+      this.remValue = (px / this.baseFontSize).toFixed(2);
+    } else {
+      this.remValue = '';
+    }
   }
+
+  convertToPx() {
+    const rem = parseFloat(this.remValue);
+    if (!isNaN(rem)) {
+      this.pxValue = (rem * this.baseFontSize).toFixed(2);
+    } else {
+      this.pxValue = '';
+    }
+  }
+
+  swapValues() {
+    [this.pxValue, this.remValue] = [this.remValue, this.pxValue];
+  }
+
+  // pxValue: number = 0;
+  // baseFontSize: number = 16;
+  // remValue: string = '';
+  // emValue: string = '';
+  // percentageValue: string = '';
+  // converted: boolean = false;
+
+  // convert() {
+  //   const px = this.pxValue || 0;
+  //   const base = this.baseFontSize || 16;
+
+  //   const rem = (px / base).toFixed(4);
+  //   const em = rem;
+  //   const percent = ((px / base) * 100).toFixed(2);
+
+  //   this.remValue = rem;
+  //   this.emValue = em;
+  //   this.percentageValue = percent;
+  //   this.converted = true;
+  // }
 }
