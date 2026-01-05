97b9a3e636d3b5cbf55e5fd83f754d22e53cfbcd
tKajal <thkkjl@gmail.com>
2026-01-05 23:04:38 +0530

    fix

--- api/ssr.ts
+++ api/ssr.ts
@@ -1,5 +1,5 @@
 // Vercel Serverless function
 import { AngularNodeApp } from '@angular/ssr/node';
-import bootstrap from '../dist/your-app/server/main.js';
+import bootstrap from '../dist/web/server/main.js';
 
 export default AngularNodeApp.bootstrap(bootstrap);
